11dc881a89f5717f6dc43c18a1bd87e9e3029aff
Vlad Pahaniuk <skipnow158@gmail.com>
2023-12-03 18:27:36 +0300

    Добавил задание №7 лабораторной работы №2

---
+++ laboratory-task-2/Laba2_7.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+bool prime(const int64_t& n) {
+	for (int64_t i = 2; i < n; i++) {
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+
+
+
+
+
+int main() {
+
+	int64_t number, prime_dist = 0, num_pair = 0;
+
+	std::cout << "Enter T pair of bliznecov\n";
+	std::cin >> number;
+
+	while (number <= 0) {
+		std::cout << "Enter natural number\n";
+		std::cin >> number;
+	}
+
+	for (size_t i = 2; num_pair < number; ++i) {
+		if (prime(i)) {
+			if (prime_dist == 1) {
+				std::cout << i - 2 << ' ' << i << '\n';
+				++num_pair;
+			}
+			prime_dist = 0;
+		}
+		else {
+			++prime_dist;
+		}
+	}
+
+}
