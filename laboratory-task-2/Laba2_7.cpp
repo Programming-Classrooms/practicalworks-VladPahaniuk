e089cddb70d28acdee56956bbc6bae184fcb2d22
Vlad Pahaniuk <skipnow158@gmail.com>
2023-12-23 17:26:57 +0300

    В задани 7 исправил табуляцию и изменил названия переменных в camelCase

--- laboratory-task-2/Laba2_7.cpp
+++ laboratory-task-2/Laba2_7.cpp
@@ -1,38 +1,43 @@
-/*Написать программу, которая для заданного натурального числа:
-7. вычисляет Т пар близнецов.*/
+/*
+Написать программу, которая для заданного натурального числа:
+7. вычисляет Т пар близнецов.
+*/
+
 
 #include <iostream>
 
-
-bool prime(const int64_t& n) {
-	for (int64_t i = 2; i < n; i++) {
+bool prime(const int64_t& n) 
+{
+	for (size_t i = 2; i < n; i++) {
 		if (n % i == 0)
 			return false;
 	}
 	return true;
 }
 
-
-
-int main() {
-	int64_t number = 0, prime_dist = 0, num_pair = 0;
+int main() 
+{
+	int64_t number = 0;
+	int64_t primeDist = 0;
+	int64_t numPair = 0;
 	std::cout << "Enter T pair of bliznecov\n";
 	std::cin >> number;
 	while (number <= 0) {
 		std::cout << "Enter natural number\n";
 		std::cin >> number;
 	}
-	for (size_t i = 2; num_pair < number; ++i) {
+	for (size_t i = 2; numPair < number; ++i) {
 		if (prime(i)) {
-			if (prime_dist == 1) {
+			if (primeDist == 1) {
 				std::cout << i - 2 << ' ' << i << '\n';
-				++num_pair;
+				++numPair;
 			}
-			prime_dist = 0;
+			primeDist = 0;
 		}
 		else {
-			++prime_dist;
+			++primeDist;
 		}
 	}
 	return 0;
 }
+
