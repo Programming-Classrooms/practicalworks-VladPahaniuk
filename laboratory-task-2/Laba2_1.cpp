baca7634b91649e77ec4792cd2d54dc50dd136a6
Vlad Pahaniuk <skipnow158@gmail.com>
2023-12-23 16:56:40 +0300

    В задании 1 добавил пробелы между оператором цикла и скобкой

--- laboratory-task-2/Laba2_1.cpp
+++ laboratory-task-2/Laba2_1.cpp
@@ -1,11 +1,10 @@
 /*Написать программу, которая для заданного натурального числа:
-1. проверяет, различны ли цифры в записи числа (массив и строки
-не использовать);*/
+1. проверяет, различны ли цифры в записи числа (массив и строкине использовать);
+*/
 
 
 #include <iostream>
 using namespace std;
-
 
 int main()
 {
@@ -13,38 +12,31 @@ int main()
     bool check = 0;
     cout << "Input number:\n";
     cin >> num;
-    if (num <= 0)
-    {
+    if (num <= 0) {
         cout << "Not a natural.";
     }
-    else
-    {
-        for (size_t i = 0; i <= 9; i++)
-        {
+    else {
+        for (size_t i = 0; i <= 9; i++) {
             temp = num;
             count = 0;
-            while (temp != 0)
-            {
-                if (temp % 10 == i)
-                {
+            while (temp != 0) {
+                if (temp % 10 == i) {
                     count++;
                 }
                 temp /= 10;
             }
-            if (count >= 2)
-            {
+            if (count >= 2){
                 check = 1;
                 break;
             }
         }
-        if (check == 1)
-        {
+        if (check == 1) {
             cout << "There are duplicates.";
         }
-        else
-        {
+        else {
             cout << "There are no duplicates.";
         }
         return 0;
     }
 }
+
